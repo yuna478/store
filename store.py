9cfad2428afb0199df117f54f65f0be92e9a3873
KINGG_trollX <yunamisaghi@yahoo.com>
2022-06-03 15:58:25 +0430

    make code more efficient

--- store.py
+++ store.py
@@ -20,23 +20,30 @@ while 1:
           "|      1- add                |\n"
           "|      2- remove             |\n"
           "|      3- show               |\n"
-          "|                            |\n"
+          "|      4- edit               |\n"
+          "|     b- back  e- exit       |\n"
           "|____________________________|\n")
-          a= input ('your choice:')
-          if a=='3':
-               print(pro)
-          elif a=='2':
-               print('under development')
-          elif a=='1':
-               while 1:
-                         a= input("enter in order: size, price ,tag, color")
-                         pro .append(a)
-                         pass
-          elif a=='E' or a=='e' :
-                         exit(0)
-          else:
-                         print("incorrect input")
-                         pass
+               a= input ('your choice:')
+               if a=='3':
+                    print(pro)
+               elif a=='2':
+                    print('under development')
+               elif a=='1':
+                    pro.append(str(len(pro)+1)+'- ')
+                    a= input("enter size: ")
+                    pro[-1]=pro[-1]+"size= "+a
+                    a= input("enter brand: ")
+                    pro[-1]=pro[-1]+", brand= "+a
+                    a= input("enter price: ")
+                    pro[-1]=pro[-1]+", price= "+a
+                    a= input("enter color: ")
+                    pro[-1]=pro[-1]+", color= "+a
+               elif a=='E' or a=='e' :
+                              exit(0)
+               else:
+                              print("incorrect input")
+                         
+                              pass
      elif a=='E' or a=='e' :
           exit(0)
      else:
